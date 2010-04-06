7a7c69dcc3a9da9290ce2ff250b45452bdefdaff
David Glick <davidglick@groundwire.org>
2010-04-06 04:22:29 +0000

    add note on what to do with this info
    svn path=/sysadmin/trunk/; revision=35771

--- sysadmin/top_ip_histogram.sh
+++ sysadmin/top_ip_histogram.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Uses dos.awk to display a histogram of IPs appearing in the most recent 1000 log entries
+
+# Once you identify the culprit, make sure it's not something important by doing a reverse
+# DNS lookup:
+# $ dig -x [IP address]
+# Then you can block the IP using an iptables rule:
+# # iptables -A INPUT -s [IP address] -j DROP
+
 tail -n 1000 /var/log/apache2/dev.plone.org/access.log /var/log/apache2/deus.plone.org/access.log /var/log/apache2/dev.plone.org/access.log /var/log/apache2/paste.plone.org/access.log /var/log/apache2/planet.plone.org/access.log /var/log/apache2/svn.plone.org/access.log | awk -f dos.awk | sort -nrk3
